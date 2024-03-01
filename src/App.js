040dabccba6c9b246c6c269ba83eddce48237c6e
Aman Patel <aman17626@gmail.com>
2024-03-01 20:36:21 +0530

    Add Tailwind CSS as a dev dependency and update component styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Head from "./components/Head";
+import Body from "./components/Body";
+import MainContainer from "./components/MainContainer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Head />
+      <Body />
+
+      {/*
+       * Head
+       * Body
+       * * Sidebar
+       *     Menu Items
+       *   MainContent
+       *     ButtonList
+       *     VideoContainer
+       *      VideoCard
+       */}
     </div>
   );
 }
---
+++ src/components/Head.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1">
+        <img
+          className="h-8"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/2048px-Hamburger_icon.svg.png"
+          alt="menu"
+        />
+        <img
+          className="h-8 mx-5"
+          src="https://vectorseek.com/wp-content/uploads/2021/01/YouTube-Logo-Vector.png"
+          alt="youtube-logo"
+        />
+      </div>
+      <div className="col-span-9 px-10">
+        <input
+          className="w-1/2 border rounded-l-full border-gray-400 p-2"
+          type="text"
+          placeholder="🔍 Search"
+        />
+        <button className="border border-l-0 border-gray-400 rounded-r-full px-5 py-2">
+          🔍
+        </button>
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2bEHaPRRyd-0qMNsPnsDsH4vmmVZReSZ95-G-TaKnog&s"
+          alt="bell"
+        />
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfyzTkEY3Uui9Tnb6aL9XX36iYGrQ_Fiy1KEn98K9ndNPXQCN5t7Tgr-7gl4szcWlhnzU&usqp=CAU"
+          alt="user"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
